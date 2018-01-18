efead536816b0c870023f3c50d65eac1b8fbb6ca
Kristian Antonsen <kba@cs.au.dk>
2018-01-18 20:17:13 +0100

    Initial implementation

---
+++ index.js
@@ -0,0 +1,4 @@
+const parse5 = require('parse5');
+const jsonmlAdapter = require('./jsonml-adapter.js');
+
+module.exports = html => parse5.parse(html, { treeAdapter: jsonmlAdapter });
---
+++ jsonml-adapter.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// Map from elements to their parents.
+const parentMap = new Map();
+
+exports.createDocument = () => [];
+exports.createDocumentFragment = () => [];
+
+exports.createElement = (tagName, namespaceURI, attrArray) => {
+	const attrs = {};
+	attrArray.forEach(({ name, value }) => attrs[name] = value);
+	return [ tagName, attrs ];
+};
+
+exports.createCommentNode = data => [ '!', data ];
+
+exports.appendChild = (parentNode, newNode) => {
+		parentMap.set(newNode, parentNode);
+		if (Array.isArray(parentNode)) parentNode.push(newNode);
+		else parentNode.push(newNode);
+};
+
+exports.insertBefore = (parentNode, newNode, referenceNode) => { 
+	throw new Error('insertBefore not implemented');
+};
+
+exports.setTemplateContent = (templateElement, contentElement) => 
+	templateElement.concat(contentElement);
+
+exports.getTemplateContent = templateElement => templateElement;
+exports.setDocumentType = (document, name, publicId, systemId) => null;
+exports.setDocumentMode = (document, mode) => null;
+exports.getDocumentMode = () => 'no-quirks';
+
+exports.insertText = (parentNode, text) => {
+	const i = parentNode.length - 1;
+	if (typeof parentNode[i] === 'string') {
+		parentNode[i] += text;
+	} else {
+		parentNode.push(text);
+	}
+};
+
+exports.insertTextBefore = (parentNode, text, referenceNode) => {
+	const i = parentNode.indexOf(referenceNode) - 1;
+	if (typeof parentNode[i] === 'string') {
+		parentNode[i] += text;
+	} else {
+		parentNode.push(text);
+	}
+};
+
+exports.adoptAttributes = (recipient, attrArray) => 
+	attrArray.forEach(({ name, value }) => recipient[1][name] = value);
+
+exports.getFirstChild = node => { throw new Error('getFirstChild not implemented'); };
+exports.getChildNodes = node => { throw new Error('getChildNodes not implemented'); };
+exports.getParentNode = node => parentMap.get(node);
+exports.getAttrList = element => typeof element[1] === 'object' ? element[1] : {};
+exports.getTagName = element => element[0];
+exports.getNamespaceURI = element => 'http://www.w3.org/1999/xhtml';
