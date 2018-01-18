28320c55d0b3692b391e441ef04eaa5622e0998d
Kristian Antonsen <kba@cs.au.dk>
2018-01-18 21:15:48 +0100

    Now returns the JsonML directly, not wrapped in an array (as it did before).

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const parse5 = require('parse5');
 const jsonmlAdapter = require('./jsonml-adapter.js');
 
-module.exports = html => parse5.parse(html, { treeAdapter: jsonmlAdapter });
+module.exports = html => parse5.parse(html, { treeAdapter: jsonmlAdapter })[0];
